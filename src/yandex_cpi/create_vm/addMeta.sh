daf7c5666731ce409373a2495958e14f7a0d38b5
root <root@jumpbox.ru-central1.internal>
2019-01-15 19:40:22 +0000

    dev

--- src/yandex_cpi/create_vm/addMeta.sh
+++
@@ -1,1 +0,0 @@
-jq .arguments[5].$1 .work/request.json > .work/meta-$1.json
---
+++ src/yandex_cpi/delete_vm/apply.sh
@@ -0,0 +1,11 @@
+VM_ID=$(jq -r '.arguments[0]' .work/request.json)
+
+yc --token $YC_PASSPORT_TOKEN \
+   --cloud-id $YC_CLOUD_ID \
+   --folder-name $YC_FOLDER_NAME \
+   --format json \
+   compute instance delete \ 
+   $VM_ID \
+   1>&2
+
+echo '{"result":"'$VM_ID'"}'
