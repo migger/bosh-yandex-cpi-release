66cd457025de778f5882b0edd21bef3aab34e64d
root <root@jumpbox.ru-central1.internal>
2019-01-15 20:40:05 +0000

    dev

--- src/yandex_cpi/delete_vm/apply.sh
+++ src/yandex_cpi/delete_vm/apply.sh
@@ -4,7 +4,7 @@ yc --token $YC_PASSPORT_TOKEN \
    --cloud-id $YC_CLOUD_ID \
    --folder-name $YC_FOLDER_NAME \
    --format json \
-   compute instance delete \ 
+   compute instance delete \
    $VM_ID \
    1>&2
 if [ "$?" == "0" ]; then
