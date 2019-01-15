1fa347a6b83b490d7a7faeaab009c93f0db3fce9
root <root@jumpbox.ru-central1.internal>
2019-01-15 20:25:03 +0000

    dev

---
+++ src/yandex_cpi/create_disk/apply.sh
@@ -0,0 +1,16 @@
+DISK_SIZE=$(jq -r '.arguments[0]' .work/request.json)
+CLOUD_PROPERTIES=$(jq -r '.arguments[1]' .work/request.json)
+VM_ID=$(jq -r '.arguments[2]' .work/request.json)
+DISK_ID=${VM_ID}_$(head /dev/urandom | tr -dc a-z0-9 | head -c 16)
+
+DISK_SIZE_GB=$(($DISK_SIZE / 1024))
+yc --token $YC_PASSPORT_TOKEN \
+   --cloud-id $YC_CLOUD_ID \
+   --folder-name $YC_FOLDER_NAME \
+   --format json \
+   compute disk create \ 
+   --name $DISK_ID \
+   --size $DISK_SIZE_GB \
+   1>&2
+
+echo '{"result":"'$DISK_ID'"}'
