b6e11ec617fa35541bda2ccd0fb5122864b7db84
root <root@jumpbox.ru-central1.internal>
2019-01-21 23:19:49 +0000

    dev

--- src/yandex_cpi/attach_disk/apply.sh
+++ src/yandex_cpi/attach_disk/apply.sh
@@ -1,6 +1,7 @@
 VM_ID=$(jq -r '.arguments[0]' .work/request.json)
 DISK_ID=$(jq -r '.arguments[1]' .work/request.json)
 
+GEN_ID=asdasd
 
 yc --token $YC_PASSPORT_TOKEN \
    --cloud-id $YC_CLOUD_ID \
@@ -16,8 +17,30 @@ yc --token $YC_PASSPORT_TOKEN \
    compute instance attach-disk \
    $VM_ID \
    --disk-name $DISK_ID \
-   --device-name ${DISK_ID} \
+   --device-name ${GEN_ID} \
    1>&2
+
+yc --token $YC_PASSPORT_TOKEN \
+   --cloud-id $YC_CLOUD_ID \
+   --folder-name $YC_FOLDER_NAME \
+   --format json \
+   compute instance get \
+   --full $VM_ID \
+   | jq -r '.metadata."user-data"' > .work/old_metadata.json
+
+cat .work/old_metadata.json 1>&2
+
+cat .work/old_metadata.json | jq '.disks.persistent."'$GEN_ID'".id="'$GEN_ID'"' > .work/new_metadata.json
+
+cat .work/new_metadata.json 1>&2
+
+yc --token $YC_PASSPORT_TOKEN \
+   --cloud-id $YC_CLOUD_ID \
+   --folder-name $YC_FOLDER_NAME \
+   --format json \
+   compute instance update \
+   $VM_ID \
+   --metadata-from-file user-data=.work/new_metadata.json
 
 yc --token $YC_PASSPORT_TOKEN \
    --cloud-id $YC_CLOUD_ID \
@@ -32,7 +55,5 @@ while [ "$CODE" != "200" ]; do
     CODE=$(curl -o /dev/null -w %{http_code} -k $YC_MBUS/agent -X POST --data {\"method\":\"ping\"})
 done
 
-TASK_ID=$(curl -k $YC_MBUS/agent -X POST \
-	--data '{"method":"add_persistent_disk","arguments":["'${DISK_ID}'",{"id":"'${DISK_ID}'"}]}' | jq -r .value.agent_task_id)
   
 echo '{}'
