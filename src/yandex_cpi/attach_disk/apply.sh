bb7908a59da927c38372b242692486c5861fd7dc
root <root@jumpbox.ru-central1.internal>
2019-01-21 09:46:23 +0000

    dev

--- src/yandex_cpi/attach_disk/apply.sh
+++ src/yandex_cpi/attach_disk/apply.sh
@@ -7,13 +7,37 @@ yc --token $YC_PASSPORT_TOKEN \
    --cloud-id $YC_CLOUD_ID \
    --folder-name $YC_FOLDER_NAME \
    --format json \
+   compute instance stop \
+   $VM_ID 
+
+yc --token $YC_PASSPORT_TOKEN \
+   --cloud-id $YC_CLOUD_ID \
+   --folder-name $YC_FOLDER_NAME \
+   --format json \
    compute instance attach-disk \
    $VM_ID \
    --disk-name $DISK_ID \
    --device-name ${PER_DISK_DEVICE_NAME} \
    1>&2
 
-curl -k https://mbus:qx6og3qijbn4e8nbilbq@10.0.0.6:6868/agent -X POST \
- --data '{"method": "add_persistent_disk", "arguments":["'${PER_DISK_DEVICE_NAME}'", {"id": "'${PER_DISK_DEVICE_NAME}'"}]}' 1>&2
+yc --token $YC_PASSPORT_TOKEN \
+   --cloud-id $YC_CLOUD_ID \
+   --folder-name $YC_FOLDER_NAME \
+   --format json \
+   compute instance start \
+   $VM_ID 
+CODE=
 
+while [ "$CODE" != "200" ]; do
+    sleep 1
+    CODE=$(curl -o /dev/null -w %{http_code} -k https://mbus:pq0m1ij9dpjmsblkl4y9@10.0.0.6:6868/agent -X POST --data {\"method\":\"ping\"})
+done
+
+TASK_ID=$(curl -k https://mbus:pq0m1ij9dpjmsblkl4y9@10.0.0.6:6868/agent -X POST \
+	--data '{"method":"add_persistent_disk","arguments":["'${PER_DISK_DEVICE_NAME}'",{"id":"'${PER_DISK_DEVICE_NAME}'"}]}' | jq -r .value.agent_task_id)
+for i in `seq 1 10`;
+do
+   curl -k https://mbus:pq0m1ij9dpjmsblkl4y9@10.0.0.6:6868/agent -X POST   --data '{"method":"get_task","arguments":["'${TASK_ID}'"]}'
+done
+  
 echo '{}'
--- src/yandex_cpi/test.sh
+++ src/yandex_cpi/test.sh
@@ -7,4 +7,5 @@ export YC_ZONE=ru-central1-a
 export YC_NETWORK=default
 export YC_SUBNETWORK=infra
 
-cat ./$1/test.json | sh -x cpi.sh $YC_PASSPORT_TOKEN $YC_CLOUD_NAME $YC_FOLDER_NAME 'cloud-foundry' 'cpi-robot' $YC_ZONE $YC_NETWORK $YC_SUBNETWORK
+cat ./$1/test.json | sh -x cpi.sh '' $YC_PASSPORT_TOKEN $YC_CLOUD_NAME $YC_FOLDER_NAME 'cloud-foundry' 'cpi-robot' $YC_ZONE $YC_NETWORK $YC_SUBNETWORK
+
