01336dcca94876104ae0239182f9ddfaccae489f
root <root@jumpbox.ru-central1.internal>
2019-01-22 07:25:08 +0000

    dev

--- src/yandex_cpi/attach_disk/apply.sh
+++ src/yandex_cpi/attach_disk/apply.sh
@@ -31,7 +31,10 @@ yc --token $YC_PASSPORT_TOKEN \
 
 cat .work/old_metadata.json 1>&2
 
-cat .work/old_metadata.json | jq '.disks.persistent."'$GEN_ID'".id="'virtio-$GEN_ID'"' > .work/new_metadata.json
+cat .work/old_metadata.json \
+	| jq '.disks.persistent."'$GEN_ID'".id="'virtio-$GEN_ID'"' \
+	| jq '.disks.persistent."'$GEN_ID'".path="'/dev/disk/by-id/virtio-$GEN_ID'"' \
+	> .work/new_metadata.json
 
 cat .work/new_metadata.json 1>&2
 
