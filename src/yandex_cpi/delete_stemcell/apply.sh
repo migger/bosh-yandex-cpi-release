e67f40e138bfdf38db6e5a6b791f1dab1b02b904
root <root@jumpbox.ru-central1.internal>
2019-02-10 19:29:53 +0000

    #6: fixed

---
+++ src/yandex_cpi/delete_stemcell/apply.sh
@@ -0,0 +1,3 @@
+STEMCELL_ID=$(jq -r '.arguments[0]' /tmp/.work/request.json)
+sh -x $(dirname $0)/delete.sh $STEMCELL_ID 1>&2
+echo '{"result": null, "error": null, "log": ""}'
---
+++ src/yandex_cpi/delete_stemcell/delete.sh
@@ -0,0 +1,6 @@
+yc --token $YC_PASSPORT_TOKEN \
+   --cloud-id $YC_CLOUD_ID \
+   --folder-name $YC_FOLDER_NAME \
+   --format json \
+   compute image delete $1 1>&2
+
