0f5b3cdf159d9dc723a6cc05989d6465ec1e42e2
Igor Mikhailyuk <migger@yandex.ru>
2019-02-26 21:58:05 +0300

    Update apply.sh

--- src/yandex_cpi/delete_stemcell/apply.sh
+++ src/yandex_cpi/delete_stemcell/apply.sh
@@ -1,4 +1,4 @@
-STEMCELL_ID=$(jq -r '.arguments[0]' /tmp/.work/request.json)
+STEMCELL_ID=$(jq -r '.arguments[0]' $WORKDIR/request.json)
 sh -x $(dirname $0)/delete.sh $STEMCELL_ID 1>&2
 
 echo '{"result": null, "error": null, "log": ""}'
