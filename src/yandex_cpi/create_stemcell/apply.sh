04e4b85d645c7a59e5c9e3f6861ca7e82a9da37d
Igor Mikhailyuk <migger@yandex.ru>
2019-02-26 20:18:31 +0300

    Update apply.sh

--- src/yandex_cpi/create_stemcell/apply.sh
+++ src/yandex_cpi/create_stemcell/apply.sh
@@ -1,6 +1,6 @@
 STEMCELL_ID=stemcell-$(uuidgen | tr '[:upper:]' '[:lower:]')
-IMAGE=$(jq -r '.arguments[0]' /tmp/.work/request.json)
-cp -v $IMAGE /tmp/.work/image  1>&2
+IMAGE=$(jq -r '.arguments[0]' $WORKDIR/request.json)
+cp -v $IMAGE $WORKDIR/image  1>&2
 sh -x $(dirname $0)/upload.sh $STEMCELL_ID 1>&2
 
 if [ $? -ne 0 ]; then
